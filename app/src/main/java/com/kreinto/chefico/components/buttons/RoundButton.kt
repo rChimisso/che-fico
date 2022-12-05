6d54b9ebf61d8de0243a45f7906a2941d66c691f
Crystal Spider <riccardo.chimisso@gmail.com>
2022-12-05 22:48:30 +0100

    Previews e SearchInput

--- app/src/main/java/com/kreinto/chefico/components/buttons/RoundButton.kt
+++ app/src/main/java/com/kreinto/chefico/components/buttons/RoundButton.kt
@@ -5,21 +5,24 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun RoundButton(
   icon: ImageVector,
+  modifier: Modifier = Modifier,
   tint: Color = Color.DarkGray,
   contentDescriptor: String = "",
   size: Dp = 32.dp,
-  modifier: Modifier = Modifier,
   onClick: () -> Unit
 ) {
   IconButton(
@@ -35,4 +38,10 @@ fun RoundButton(
       tint = tint
     )
   }
+}
+
+@Composable
+@Preview()
+fun RoundButtonPreview() {
+  RoundButton(icon = Icons.Default.Add) {}
 }
