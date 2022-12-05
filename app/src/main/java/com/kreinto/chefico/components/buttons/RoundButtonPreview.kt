521e4d43abcd82430d72cbc79e5538fd75ef1fe9
Crystal Spider <riccardo.chimisso@gmail.com>
2022-12-05 17:33:00 +0100

    RoundButtonPreview

---
+++ app/src/main/java/com/kreinto/chefico/components/buttons/RoundButtonPreview.kt
@@ -0,0 +1,12 @@
+package com.kreinto.chefico.components.buttons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview()
+fun RoundButtonPreview() {
+  RoundButton(icon = Icons.Default.Add) {}
+}
