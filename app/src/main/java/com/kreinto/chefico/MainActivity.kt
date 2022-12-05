7be57dff1df309d7b0276a6c1c7d2ec836426278
FriedFridge <FriedFridge@nowhere.else>
2022-12-05 17:28:25 +0100

    RoundButton

--- app/src/main/java/com/kreinto/chefico/MainActivity.kt
+++ app/src/main/java/com/kreinto/chefico/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kreinto.chefico
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,6 +13,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kreinto.chefico.components.buttons.RoundButton
 import com.kreinto.chefico.ui.theme.CheFicoTheme
 
@@ -19,14 +21,16 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      CheFicoTheme { // A surface container using the 'background' color from the theme
-        Surface(
-          modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
-        ) {
+      Row() {
+        RoundButton(icon = Icons.Default.Add) {
 
-          RoundButton(icon = Icons.Default.Add)
+        }
+        RoundButton(icon = Icons.Default.Add, size = 20.dp) {
+
         }
       }
     }
   }
 }
+
+
--- app/src/main/java/com/kreinto/chefico/components/buttons/RoundButton.kt
+++ app/src/main/java/com/kreinto/chefico/components/buttons/RoundButton.kt
@@ -1,5 +1,6 @@
 package com.kreinto.chefico.components.buttons
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
@@ -7,21 +8,31 @@ import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun RoundButton(icon: ImageVector, tint: Color = Color.Green) {
+fun RoundButton(
+  icon: ImageVector,
+  tint: Color = Color.Green,
+  contentDescriptor: String = "",
+  size: Dp = 60.dp,
+  onClick: () -> Unit
+) {
   IconButton(
-    modifier = Modifier.then(Modifier.size(50.dp)).clip(CircleShape),
-    onClick = {
-
-    },
+    modifier = Modifier
+      .size(size)
+      .clip(CircleShape)
+      .background(Color.White),
+    onClick = onClick
   ) {
     Icon(
       imageVector = icon,
-      contentDescription =  "",
+      contentDescription =  contentDescriptor,
       tint = tint
     )
   }
