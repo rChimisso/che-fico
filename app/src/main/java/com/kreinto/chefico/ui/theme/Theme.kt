fb8e52ebc2f7b2060f97b14c2336ff53174a2745
Your Name <you@example.com>
2023-01-02 15:40:21 +0100

    02/01/2023 colors

--- app/src/main/java/com/kreinto/chefico/ui/theme/Theme.kt
+++ app/src/main/java/com/kreinto/chefico/ui/theme/Theme.kt
@@ -6,12 +6,12 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-  primary = Purple200, primaryVariant = Purple700, secondary = Teal200
+private val LightColorPalette = lightColors(
+  primary = primaryColor, primaryVariant = primaryLightColor, secondary = secondaryLightColor
 )
 
-private val LightColorPalette = lightColors(
-  primary = Purple500, primaryVariant = Purple700, secondary = Teal200
+private val DarkColorPalette = darkColors(
+  primary = primaryColor, primaryVariant = primaryDarkColor, secondary = secondaryDarkColor
 
   /* Other default colors to override
   background = Color.White,
