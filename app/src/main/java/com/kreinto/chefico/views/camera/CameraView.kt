171061409c36bfc1783d779a91fe88fbb507f4b4
albeinfo <a.ricci21@campus.unimib.it>
2023-03-11 11:17:20 +0100

    scommentato codice

--- app/src/main/java/com/kreinto/chefico/views/camera/CameraView.kt
+++ app/src/main/java/com/kreinto/chefico/views/camera/CameraView.kt
@@ -5,12 +5,24 @@ import android.util.Log
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import com.kreinto.chefico.R
+import com.kreinto.chefico.Route
+import com.kreinto.chefico.components.buttons.FilledButton
+import com.kreinto.chefico.components.buttons.data.ButtonData
+import com.kreinto.chefico.components.frames.SimpleFrame
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.util.concurrent.Executor
@@ -47,7 +59,7 @@ fun CameraView(onNavigate: (route: String) -> Unit) {
   val fileName = remember { mutableStateOf("") }
 
 
-  /*SimpleFrame(
+  SimpleFrame(
     onBackPressed = onNavigate,
     bottomBar = {
       Row(
@@ -81,7 +93,6 @@ fun CameraView(onNavigate: (route: String) -> Unit) {
   if (fileName.value != "") {
     onNavigate(Route.PlantDetail.route(fileName.value))
   }
-}*/
 }
 
 
