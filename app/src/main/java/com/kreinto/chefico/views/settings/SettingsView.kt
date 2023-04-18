84e6829f20b6e45e50a98e9ded82d67ff7f0b5d2
Your Name <you@example.com>
2023-04-18 18:13:37 +0200

    SettingsView changes

--- app/src/main/java/com/kreinto/chefico/views/settings/SettingsView.kt
+++ app/src/main/java/com/kreinto/chefico/views/settings/SettingsView.kt
@@ -1,13 +1,21 @@
 package com.kreinto.chefico.views.settings
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -17,14 +25,16 @@ import com.kreinto.chefico.components.buttons.FilledButton
 import com.kreinto.chefico.components.frames.SimpleFrame
 import com.kreinto.chefico.components.frames.StandardFrame
 import com.kreinto.chefico.room.AuthViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @ExperimentalMaterial3Api
 @Composable
-
-//bugg con l'icona di back, a volte funziona a volte no rimanendo nel loop del menu delle impostazioni e della view per registrarsi
-
 fun SettinsView(onNavigate: (String) -> Unit, authViewModel: AuthViewModel) {
+  val context = LocalContext.current
   StandardFrame(
     onNavPressed = onNavigate,
     title = {
@@ -32,7 +42,7 @@ fun SettinsView(onNavigate: (String) -> Unit, authViewModel: AuthViewModel) {
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
-      ){
+      ) {
         if (authViewModel.isUserLoggedIn()) {
           // TODO: cambiare recupero nome utente e visualizzazione ID (#&&&&&& tipo discord).
           Text("${Firebase.auth.currentUser?.displayName}")
@@ -52,98 +62,83 @@ fun SettinsView(onNavigate: (String) -> Unit, authViewModel: AuthViewModel) {
       }
     },
     bottomBar = {
-      Column(
+      Row(
         modifier = Modifier
+          .height(80.dp)
           .fillMaxWidth()
-          .fillMaxHeight(),
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.CenterHorizontally
+          .drawBehind {
+            drawLine(color = Color.White, start = Offset.Zero, end = Offset(size.width, 0f))
+          }
+          .padding(BottomAppBarDefaults.ContentPadding),
+        horizontalArrangement = Arrangement.spacedBy(space = 8.dp, alignment = Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically
       ) {
-        Divider()
-        ListItem(
-          //list item per le info delll'app ecc... da mettere in fondo alla pagina
-          headlineContent = { Text("INFO") },
-        )
+        Text("Privacy")
+        Text("·")
+        Text("Copyright")
+        Text("·")
+        Text("Versione")
       }
     }
   ) {
+    var automaticDeletion by rememberSaveable { mutableStateOf(false) }
     Column(
       modifier = Modifier
         .padding(top = it.calculateTopPadding())
-        .fillMaxWidth()
-        .fillMaxHeight(),
-      //verticalArrangement = Arrangement.Center,
-      horizontalAlignment = Alignment.CenterHorizontally
+        .fillMaxSize()
     ) {
-      //Spacer(modifier = Modifier.height(50.dp))
-      ListItem(
-        //singolo item
-        headlineContent = {
-          Button(
-            onClick = {
-              if (authViewModel.isUserLoggedIn()) {
-                onNavigate(CheFicoRoute.Account.path)
-              } else {
-                onNavigate(CheFicoRoute.Login.path)
-              }
-            },
-            contentPadding = ButtonDefaults.ButtonWithIconContentPadding
-          ) {
-            if (authViewModel.isUserLoggedIn()) {
-              Text("Impostazioni Account")
-            } else {
-              Text("Registrati")
-            }
-          }
-        },
-      )
-      Divider()//linea divisiva
-      Column(
+      Row(
         modifier = Modifier
           .fillMaxWidth()
-          .fillMaxHeight(),
-        //verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+          .clickable { automaticDeletion = !automaticDeletion }
+          .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
       ) {
-        ListItem(
-          headlineContent = { Text("One ") },
-          leadingContent = {
-            Icon(
-              Icons.Filled.Favorite,
-              contentDescription = "Localized description",
-            )
-          }
-        )
-        ListItem(
-          headlineContent = { Text("Two") },
-          leadingContent = {
-            Icon(
-              Icons.Filled.Favorite,
-              contentDescription = "Localized description",
-            )
-          }
-        )
-        ListItem(
-          headlineContent = { Text("Three") },
-          leadingContent = {
-            Icon(
-              Icons.Filled.Favorite,
-              contentDescription = "Localized description",
-            )
-          }
-        )
-        ListItem(
-          headlineContent = { Text("Four") },
-          leadingContent = {
-            Icon(
-              Icons.Filled.Favorite,
-              contentDescription = "Localized description",
-            )
-          }
-        )
-
+        Text("Autoeliminazione notifiche (30gg)")
+        Switch(checked = automaticDeletion, onCheckedChange = { checked -> automaticDeletion = checked })
       }
-
+      Row(
+        modifier = Modifier
+          .fillMaxWidth()
+          .clickable {
+            val alertBuilder = android.app.AlertDialog.Builder(context)
+            alertBuilder.setCancelable(true)
+            alertBuilder.setTitle("Eliminazione Notifiche")
+            alertBuilder.setMessage("Vuoi eliminare tutte le notifiche?")
+            alertBuilder.setPositiveButton("Sì") { _, _ -> }
+            alertBuilder.setNegativeButton("No") { _, _ -> }
+            val alert = alertBuilder.create()
+            alert.show()
+          }
+          .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+      ) {
+        Text("Elimina notifiche", modifier = Modifier
+          .fillMaxWidth(),)
+      }
     }
+//      ListItem(
+//        //singolo item
+//        headlineContent = {
+//          Button(
+//            onClick = {
+//              if (authViewModel.isUserLoggedIn()) {
+//                onNavigate(CheFicoRoute.Account.path)
+//              } else {
+//                onNavigate(CheFicoRoute.Login.path)
+//              }
+//            },
+//            contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+//          ) {
+//            if (authViewModel.isUserLoggedIn()) {
+//              Text("Impostazioni Account")
+//            } else {
+//              Text("Registrati")
+//            }
+//          }
+//        },
+//      )
   }
 }
