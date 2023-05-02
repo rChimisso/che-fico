1398c6b9bef96e4a196dac538ae7bdeafb0e7da5
Your Name <you@example.com>
2023-05-02 17:58:18 +0200

    SettingsView changes

--- app/src/main/java/com/kreinto/chefico/views/settings/SettingsView.kt
+++ app/src/main/java/com/kreinto/chefico/views/settings/SettingsView.kt
@@ -1,15 +1,18 @@
 package com.kreinto.chefico.views.settings
 
+import android.R.string
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,16 +28,16 @@ import com.kreinto.chefico.components.buttons.FilledButton
 import com.kreinto.chefico.components.frames.SimpleFrame
 import com.kreinto.chefico.components.frames.StandardFrame
 import com.kreinto.chefico.room.AuthViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @ExperimentalMaterial3Api
 @Composable
 fun SettinsView(onNavigate: (String) -> Unit, authViewModel: AuthViewModel) {
   val context = LocalContext.current
+  var language by remember { mutableStateOf("Italiano") }
   StandardFrame(
     onNavPressed = onNavigate,
     title = {
@@ -126,37 +129,77 @@ fun SettinsView(onNavigate: (String) -> Unit, authViewModel: AuthViewModel) {
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
       ) {
-          Text("Lingua")
-          Box {
-            Text("Italiano", modifier = Modifier.align(Alignment.Center))
-            DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
-              DropdownMenuItem(text = {Text("Italiano")}, onClick = {})
-              DropdownMenuItem(text = {Text("Inglese")}, onClick = {})
-            }
+        Text("Lingua")
+        Box {
+          Text(text = "${language}", modifier = Modifier.align(Alignment.Center))
+          DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
+            DropdownMenuItem(text = { Text("Italiano") }, onClick = { language = "Italiano" })
+            DropdownMenuItem(text = { Text("Inglese") }, onClick = { language = "Inglese" })
           }
+        }
 
       }
+
+      val radioOptions = listOf("Chi", "Scu", "Def")
+      val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[2]) }
+      Row(
+        modifier = Modifier
+          .fillMaxWidth()
+          .height(IntrinsicSize.Min)
+          .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+      ) {
+        Column(
+//          modifier = Modifier
+//            .fillMaxHeight(),
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.Center
+        ) {
+          Text("Tema:")
+        }
+        Row(
+          modifier = Modifier
+            .fillMaxHeight(),
+          horizontalArrangement = Arrangement.Center,
+          verticalAlignment = Alignment.CenterVertically
+        ) {
+          radioOptions.forEach { text ->
+            RadioButton(
+              selected = (text == selectedOption),
+              onClick = { onOptionSelected(text) }
+            )
+            Text(
+              text = text,
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+          }
+        }
+      }
+      Row(
+        modifier = Modifier
+          .fillMaxSize()
+          .padding(bottom = it.calculateBottomPadding() + 16.dp, start = 16.dp, top = 16.dp, end = 16.dp),
+      horizontalArrangement = Arrangement.Center,
+      verticalAlignment = Alignment.Bottom
+      ) {
+        Button(
+          onClick = {
+            if (authViewModel.isUserLoggedIn()) {
+              onNavigate(CheFicoRoute.Account.path)
+            } else {
+              onNavigate(CheFicoRoute.Login.path)
+            }
+          },
+          contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+        ) {
+          if (authViewModel.isUserLoggedIn()) {
+            Text("Impostazioni Account")
+          } else {
+            Text("Registrati")
+          }
+        }
+      }
     }
-//      ListItem(
-//        //singolo item
-//        headlineContent = {
-//          Button(
-//            onClick = {
-//              if (authViewModel.isUserLoggedIn()) {
-//                onNavigate(CheFicoRoute.Account.path)
-//              } else {
-//                onNavigate(CheFicoRoute.Login.path)
-//              }
-//            },
-//            contentPadding = ButtonDefaults.ButtonWithIconContentPadding
-//          ) {
-//            if (authViewModel.isUserLoggedIn()) {
-//              Text("Impostazioni Account")
-//            } else {
-//              Text("Registrati")
-//            }
-//          }
-//        },
-//      )
   }
 }
