7fc00322ffa50664e450fc8691379187c3ca8443
albeinfo <a.ricci21@campus.unimib.it>
2023-05-16 19:13:09 +0200

    refractor

--- app/src/main/java/com/kreinto/chefico/views/account/blacklist/BlackListView.kt
+++ app/src/main/java/com/kreinto/chefico/views/account/blacklist/BlackListView.kt
@@ -7,26 +7,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.kreinto.chefico.CheFicoRoute
 import com.kreinto.chefico.R
-import com.kreinto.chefico.components.buttons.data.ButtonData
-import com.kreinto.chefico.components.frames.SimpleFrame
-import com.kreinto.chefico.components.frames.StandardFrame
-import com.kreinto.chefico.components.frames.bottombars.SimpleBottomBar
-import com.kreinto.chefico.components.inputs.SearchInput
-import com.kreinto.chefico.components.items.SelectableItem
-import com.kreinto.chefico.room.AuthViewModel
-import com.kreinto.chefico.room.CheFicoViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import com.kreinto.chefico.components.buttons.TransparentButton
+import com.kreinto.chefico.components.frames.StandardFrame
 import com.kreinto.chefico.components.items.SwipeableItem
+import com.kreinto.chefico.room.AuthViewModel
 
 @ExperimentalMaterialApi
 @ExperimentalFoundationApi
@@ -36,16 +26,13 @@ fun BlackListView(onNavigate: (String) -> Unit, authViewModel: AuthViewModel) {
   var blackList by rememberSaveable {
     mutableStateOf(emptyList<Map.Entry<String, String>>())
   }
-  LaunchedEffect(Unit){
+  LaunchedEffect(Unit) {
     authViewModel.getBlockedUsers { blackList = it.entries.toList() }
   }
 
-  val selectedAccounts = remember { mutableStateListOf<String>() }
-  var filter: String by rememberSaveable { mutableStateOf("") }
-
   StandardFrame(
     onNavPressed = onNavigate,
-    title = { SearchInput(onValueChange = { query -> filter = query }) },
+    title = { Text("Utenti bloccati") },
   ) {
     LazyColumn(
       modifier = Modifier
@@ -63,9 +50,9 @@ fun BlackListView(onNavigate: (String) -> Unit, authViewModel: AuthViewModel) {
               colors = IconButtonDefaults.iconButtonColors(
                 contentColor = Color(0xFFFFC107)
               )
-            ) {authViewModel.unblockUser(blackList[index].key)}
+            ) { authViewModel.unblockUser(blackList[index].key) }
           }),
-        ){}
+        ) {}
       }
     }
   }
